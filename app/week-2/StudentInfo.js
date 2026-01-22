9118c095bfa95a2655276fcc100d97f3855b576b
Manpreet Singh <ManpreetSingh3@edu.sait.ca>
2026-01-21 18:24:43 -0700

    page.js done and StudentInfo.js done

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StundentInfo(){
+    return(
+        <div>
+            <p>Name - Manpreet Singh</p>
+            <p>
+                <Link href ="https://github.com/Manpreet445/cprg306-assignments">Manprert Singh Github</Link>
+            </p>
+        </div>
+    );
+}
