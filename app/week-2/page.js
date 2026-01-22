d8d5455450ecd32aef4383d75cc2ed6ed829a796
Manpreet Singh <ManpreetSingh3@edu.sait.ca>
2026-01-21 18:30:59 -0700

    page.js updated

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,7 +3,7 @@ import StudentInfo from './StudentInfo.js';
 export default function Page() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <h1>Assignment 2/week 2 - WEB DEV</h1>
       <StudentInfo/>
     </main>
   );
