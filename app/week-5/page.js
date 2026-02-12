296210fb6d67e4f5ee6c8aaa6e8422b13296cb85
Manpreet Singh <ManpreetSingh3@edu.sait.ca>
2026-02-12 14:12:07 -0700

    week 5

---
+++ app/week-5/page.js
@@ -0,0 +1,8 @@
+import NewItem from "./NewItem";
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-black flex justify-center items-start pt-10">
+      <NewItem />
+    </main>
+  );
+}
